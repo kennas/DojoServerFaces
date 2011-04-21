bcdb4656394a5ef16f991fa971d94198b379c8a7
Xiaoming Zhang <zhangzxm@cn.ibm.com>
2011-04-21 18:09:45 +0800

    Fix defect #9833 - The attribute "actionListener" of dojo:button doesn't work with f:ajax

--- dojoserverfaces-library-definition/src/main/java/org/dojoserverfaces/component/html/form/Select.java
+++ dojoserverfaces-library-definition/src/main/java/org/dojoserverfaces/component/html/form/Select.java
@@ -5,8 +5,17 @@
  *******************************************************************************/
 package org.dojoserverfaces.component.html.form;
 
+import java.util.Collection;
+
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.validator.Validator;
+import javax.faces.validator.ValidatorException;
+
 import org.dojoserverfaces.build.annotation.Property;
 import org.dojoserverfaces.build.annotation.SelectValueHolder;
+import org.dojoserverfaces.component.html.store.SelectItemStoreComponent;
+import org.dojoserverfaces.widget.DojoWidget;
 import org.dojoserverfaces.widget.property.DojoRefProperty;
 import org.dojoserverfaces.widget.property.StringArrayProperty;
 
@@ -19,50 +28,97 @@ import org.dojoserverfaces.widget.property.StringArrayProperty;
 @SelectValueHolder(dojoType = "dijit.form.Select", displayName = "Select Listbox")
 class Select extends InputBase {
 
-    /**
-     * Indicates a value is required to be submitted for this component.
-     */
-    @Property(handler = RequiredProperty.class)
-    Boolean required;
+	public static class DataStoreRefProperty extends DojoRefProperty implements
+			Validator {
 
-    /**
-     * Currently error/prompt message. When using the default tooltip
-     * implementation, this will only be displayed when the field is focused.
-     */
-    @Property
-    String message;
-    // TODO does this conflict with common tooltip attribute?
+		public DataStoreRefProperty(String name) {
+			super(name);
+		}
 
-    /**
-     * This attribute controls the position of the tooltip. It can be a list of
-     * the following values:
-     * 
-     * before: places tooltip to the left of the component, or to the right in
-     * the case of RTL scripts like Hebrew and Arabic
-     * 
-     * after: places tooltip to the right of the target node/widget, or to the
-     * left in the case of RTL scripts like Hebrew and Arabic
-     * 
-     * above: tooltip goes above target node
-     * 
-     * below: tooltip goes below target node
-     * 
-     * The list, (String[] or a String where the values are space separated, of
-     * positions is tried, in order, until a position is found where the tooltip
-     * fits within the viewport. Be careful setting this attribute. A value of
-     * "above" may work fine until the user scrolls the screen so that there's
-     * no room above the target node. Nodes with drop downs, like DropDownButton
-     * or FilteringSelect, are especially problematic, in that you need to be
-     * sure that the drop down and tooltip don't overlap, even when the viewport
-     * is scrolled so that there is only room below (or above) the target node,
-     * but not both.
-     */
-    @Property(handler = StringArrayProperty.class)
-    Object tooltipPosition;
-    /**
-     * Id of a data store component containing potential values for the field.e.g.SelectItemStore
-     */
-    @Property(handler = DojoRefProperty.class)
-    Object store;
+		public DataStoreRefProperty(String attributeName, String propertyName) {
+			super(attributeName, propertyName);
+		}
 
+		/*
+		 * @see
+		 * javax.faces.validator.Validator#validate(javax.faces.context.FacesContext
+		 * , javax.faces.component.UIComponent, java.lang.Object)
+		 */
+		@Override
+		public void validate(FacesContext context, UIComponent comp,
+				Object value) throws ValidatorException {
+			String storeId = (String) comp.getAttributes().get(this.getName());
+			if (storeId == null || value == null)
+				return;
+
+			// TODO: Cannot use UIComponent as the type, it will cause
+			// eclipse error prompt, need to improve?
+			UIComponent storeComp = context.getViewRoot().findComponent(storeId);
+			if (storeComp != null && (storeComp instanceof SelectItemStoreComponent)) {
+				Validator validator = getDataStroeValidator((SelectItemStoreComponent) storeComp);
+				if (validator != null) {
+					validator.validate(context, (UIComponent) storeComp, value);
+				}
+			}
+		}
+
+		private Validator getDataStroeValidator(SelectItemStoreComponent selectItemStore) {
+			Collection<org.dojoserverfaces.widget.property.Property> propertyHandlers = ((DojoWidget) selectItemStore)
+					.getPropertyHandlers();
+			for (org.dojoserverfaces.widget.property.Property property : propertyHandlers) {
+				if ("data".equals(property.getName())
+						&& property instanceof Validator) {
+					return (Validator) property;
+				}
+			}
+			return null;
+		}
+	}
+
+	/**
+	 * Indicates a value is required to be submitted for this component.
+	 */
+	@Property(handler = RequiredProperty.class)
+	Boolean required;
+
+	/**
+	 * Currently error/prompt message. When using the default tooltip
+	 * implementation, this will only be displayed when the field is focused.
+	 */
+	@Property
+	String message;
+	// TODO does this conflict with common tooltip attribute?
+
+	/**
+	 * This attribute controls the position of the tooltip. It can be a list of
+	 * the following values:
+	 * 
+	 * before: places tooltip to the left of the component, or to the right in
+	 * the case of RTL scripts like Hebrew and Arabic
+	 * 
+	 * after: places tooltip to the right of the target node/widget, or to the
+	 * left in the case of RTL scripts like Hebrew and Arabic
+	 * 
+	 * above: tooltip goes above target node
+	 * 
+	 * below: tooltip goes below target node
+	 * 
+	 * The list, (String[] or a String where the values are space separated, of
+	 * positions is tried, in order, until a position is found where the tooltip
+	 * fits within the viewport. Be careful setting this attribute. A value of
+	 * "above" may work fine until the user scrolls the screen so that there's
+	 * no room above the target node. Nodes with drop downs, like DropDownButton
+	 * or FilteringSelect, are especially problematic, in that you need to be
+	 * sure that the drop down and tooltip don't overlap, even when the viewport
+	 * is scrolled so that there is only room below (or above) the target node,
+	 * but not both.
+	 */
+	@Property(handler = StringArrayProperty.class)
+	Object tooltipPosition;
+	/**
+	 * Id of a data store component containing potential values for the
+	 * field.e.g.SelectItemStore
+	 */
+	@Property(handler = DataStoreRefProperty.class)
+	Object store;
 }
