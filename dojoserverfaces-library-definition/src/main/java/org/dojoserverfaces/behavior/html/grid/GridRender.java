86046fd6d3066e4d73a4504823bc91634599b265
Bing Dong <dongbing1010@msn.com>
2011-05-13 17:15:17 +0800

    add a Grid refresh behavior

---
+++ dojoserverfaces-library-definition/src/main/java/org/dojoserverfaces/behavior/html/grid/GridRender.java
@@ -0,0 +1,15 @@
+package org.dojoserverfaces.behavior.html.grid;
+
+import org.dojoserverfaces.build.annotation.Behavior;
+/**
+ * Refresh grid
+ *
+ */
+@Behavior
+public class GridRender extends SimpleGridOperationBase {
+
+    public GridRender() {
+        super("render");
+    }
+
+}
