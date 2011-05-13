683e4e4b9b254a49007e6df9d700356f5191d7c0
Bing Dong <dongbing1010@msn.com>
2011-05-13 17:40:55 +0800

    a custom simple jsonObject

--- dojoserverfaces-library-showcase/src/main/java/org/dojoserverfaces/showcase/rest/JSONObject.java
+++ dojoserverfaces-library-showcase/src/main/java/org/dojoserverfaces/showcase/rest/JSONObject.java
@@ -3,18 +3,22 @@ package org.dojoserverfaces.showcase.rest;
 import java.util.HashMap;
 import java.util.Map;
 
+/**
+ * Simple JSONObject
+ * 
+ */
 public class JSONObject {
     private Map<Object, Object> jsonObj = new HashMap<Object, Object>();
 
-    public void put(Object key, Object value) {
-        String skey = "\""+key+"\"";
-        String sv = "\""+value+"\"";
-        jsonObj.put(skey, sv);
+    void put(Object key, Object value) {
+        jsonObj.put("\"" + key + "\"", "\"" + value + "\"");
     }
-    public String toString(){
+
+    public String toString() {
         return jsonObj.toString().replace("=", ":");
     }
-    public Object get(Object key){
+
+    Object get(Object key) {
         return jsonObj.get(key);
     }
 
