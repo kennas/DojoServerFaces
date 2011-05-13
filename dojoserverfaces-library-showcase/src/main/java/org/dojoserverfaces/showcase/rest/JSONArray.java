6eae04384f4ba3a41534c9b4ebff44d773b10f86
Bing Dong <dongbing1010@msn.com>
2011-05-13 17:52:58 +0800

    added copyright header

--- dojoserverfaces-library-showcase/src/main/java/org/dojoserverfaces/showcase/rest/JSONArray.java
+++ dojoserverfaces-library-showcase/src/main/java/org/dojoserverfaces/showcase/rest/JSONArray.java
@@ -1,3 +1,8 @@
+/*******************************************************************************
+ *      Copyright (c) 2011, The Dojo Foundation All Rights Reserved.
+ *      Available via Academic Free License >= 2.1 OR the modified BSD license.
+ *      see: http://dojotoolkit.org/license for details
+ *******************************************************************************/
 package org.dojoserverfaces.showcase.rest;
 
 import java.util.ArrayList;
