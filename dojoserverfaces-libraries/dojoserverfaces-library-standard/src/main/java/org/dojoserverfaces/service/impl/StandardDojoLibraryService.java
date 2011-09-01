f9635336c92f518ebd159df67b3f23d5cdf7954c
Xiaoming Zhang <zhangzxm@cn.ibm.com>
2011-09-01 18:14:14 +0800

    Add javadoc and copyright standard library service and theme handler.

--- dojoserverfaces-libraries/dojoserverfaces-library-standard/src/main/java/org/dojoserverfaces/service/impl/StandardDojoLibraryService.java
+++ dojoserverfaces-libraries/dojoserverfaces-library-standard/src/main/java/org/dojoserverfaces/service/impl/StandardDojoLibraryService.java
@@ -1,8 +1,16 @@
+/*******************************************************************************
+ *      Copyright (c) 2011, The Dojo Foundation All Rights Reserved.
+ *      Available via Academic Free License >= 2.1 OR the modified BSD license.
+ *      see: http://dojotoolkit.org/license for details
+ *******************************************************************************/
 package org.dojoserverfaces.service.impl;
 
 import org.dojoserverfaces.service.DojoThemeHandler;
 import org.dojoserverfaces.service.LibraryService;
 
+/**
+ * Library Service for DojoServiceFaces Standard Library
+ */
 public class StandardDojoLibraryService extends LibraryService {
     private DojoThemeHandler themeHandler = new StandardDojoThemeHandler();
 
